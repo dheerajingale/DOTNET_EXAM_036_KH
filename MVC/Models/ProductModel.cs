03b4ef592e2c87c57d328e947dadb85843ffd8ab
dheerajingale <96038295+dheerajingale@users.noreply.github.com>
2022-02-19 17:35:28 +0530

    Add files via upload

---
+++ MVC/Models/ProductModel.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ProductMVCExam.Models
+{
+    public class ProductModel
+    {
+        [Display(Name = "ProductId")]
+        public int ProductId { get; set; }
+
+        [DataType(DataType.Text)]
+        [Required(ErrorMessage ="Please Enter Product Name")]
+        [Display(Name = "ProductName")]
+        public string ProductName { get; set; }
+
+        [DataType(DataType.Currency)]
+        [Required(ErrorMessage = "Please Enter Rate of Product")]
+        [Display(Name = "Rate")]
+        public decimal Rate { get; set; }
+
+        [DataType(DataType.Text)]
+        [Required(ErrorMessage = "Please give Description")]
+        [Display(Name = "Description")]
+        public string Description { get; set; }
+
+        [DataType(DataType.Text)]
+        [Required(ErrorMessage = "Please Enter Appropriate Category Name")]
+        [Display(Name = "CategoryName")]
+        public string CategoryName { get; set; }
+    }
+}
